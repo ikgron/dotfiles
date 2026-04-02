7e16e18d94aad35ff0eff6b2602e5cb3266b2ad8
parser <boogie-rover.0a@icloud.com>
2026-04-02 12:04:56 -0500

    Update debian/install.sh

--- debian/install.sh
+++ debian/install.sh
@@ -4,7 +4,7 @@
 # update, upgrade, and install apps
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y alacritty btop eza fastfetch nodejs npm starship
+sudo apt install -y alacritty btop eza fastfetch gamemode nodejs npm starship
 
 # recommended zed install
 curl -f https://zed.dev/install.sh | sh
@@ -13,3 +13,7 @@ curl -f https://zed.dev/install.sh | sh
 mkdir -p ~/.local/share/fonts
 wget -P ~/.local/share/fonts/ https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/FiraCode/Regular/FiraCodeNerdFontMono-Regular.ttf
 fc-cache -f
+
+# https://gitlab.com/Schwarzer_Kater
+# https://github.com/DavidoTek/ProtonUp-Qt
+# https://github.com/VirusAlex/elden-proton
