3eec1b887acebcca3440a2682f82c130c6227c05
parser <boogie-rover.0a@icloud.com>
2026-04-29 17:13:25 -0500

    Update macos/defaults.sh

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -1,17 +1,14 @@
+#!/usr/bin/env bash
+
 COMPUTER_NAME="ThinkPad"
-SCREENSHOTS_FOLDER="${HOME}/Screenshots"
 
 osascript -e 'tell application "System Settings" to quit'
 
 # Ask for administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until this script has finished
-while "true"; do
-  sudo -n "true"
-  sleep 60
-  kill -0 "$$" || exit
-done 2>/dev/null &
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
 # Computer & Host name                                                        #
@@ -24,118 +21,20 @@ sudo scutil --set LocalHostName "$COMPUTER_NAME"
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$COMPUTER_NAME"
 
 ###############################################################################
-# System                                                                      #
-###############################################################################
-
-# Menu bar: show battery percentage
-defaults write com.apple.menuextra.battery ShowPercent YES
-
-# Disable opening and closing window animations
-defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool "false"
-
-# Save to disk (not to iCloud) by default
-defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool "false"
-
-# Group windows by application
-defaults write com.apple.dock "expose-group-apps" -bool "true"
-
-# Disable rich text
-defaults write com.apple.TextEdit "RichText" -bool "false"
-
-###############################################################################
-# Keyboard & Input                                                            #
-###############################################################################
-
-# Disable typing automatic capitalization.
-defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool "false"
-
-# Disable typing automatic period substition a.k.a. "smart stops".
-defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -int 0
-
-# Disable typing automatic dash substitution e.g. "smart dashes".
-defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool "false"
-
-# Disable typing automatic quote substitution a.k.a. "smart quotes".
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool "false"
-
-# Disable typing automatic spelling correction a.k.a. "auto-correct".
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool "false"
-
-# Enable full keyboard access for all controls
-# (e.g. enable Tab in modal dialogs)
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
-# Disable press-and-hold for keys in favor of key repeat
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-
-###############################################################################
-# Trackpad, mouse, Bluetooth accessories                                      #
-###############################################################################
-
-# Tap to click for this user
-defaults write com.apple.AppleMultitouchTrackpad Clicking -bool "true"
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-
-# Set language and text formats
-defaults write NSGlobalDomain AppleLanguages -array "en" "nl"
-defaults write NSGlobalDomain AppleLocale -string "en_US@currency=USD"
-defaults write NSGlobalDomain AppleMeasurementUnits -string "Inches"
-defaults write NSGlobalDomain AppleMetricUnits -bool "false"
-
-###############################################################################
-# Screen                                                                      #
-###############################################################################
-
-# Require password immediately after sleep or screen saver begins
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
-
-# Save screenshots to the ~/Screenshots folder
-mkdir -p "${SCREENSHOTS_FOLDER}"
-defaults write com.apple.screencapture location -string "${SCREENSHOTS_FOLDER}"
-
-# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
-defaults write com.apple.screencapture type -string "png"
-
-# Disable shadow in screenshots
-defaults write com.apple.screencapture disable-shadow -bool "true"
-
-###############################################################################
 # Finder                                                                      #
 ###############################################################################
 
 # Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
-defaults write com.apple.finder QuitMenuItem -bool "true"
+defaults write com.apple.finder QuitMenuItem -bool true
 
-# Finder: show hidden files by default
-defaults write com.apple.finder AppleShowAllFiles -bool "true"
-
-# Finder: show all filename extensions
-defaults write NSGlobalDomain AppleShowAllExtensions -bool "true"
-
-# Avoid creating .DS_Store files on network
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool "true"
-
-###############################################################################
-# Dock                                                                        #
-###############################################################################
-
-# Don’t animate opening applications from the Dock
-defaults write com.apple.dock launchanim -bool "false"
-
-# Automatically hide and show the Dock
-defaults write com.apple.dock autohide -bool "true"
-
-# Make Dock icons of hidden applications translucent
-defaults write com.apple.dock showhidden -bool "true"
-
-# Don't show recently used applications in the Dock
-defaults write com.apple.dock show-recents -bool "false"
+# Avoid creating .DS_Store files on network and USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
 ###############################################################################
 # Kill affected applications                                                  #
 ###############################################################################
 
-for app in "Calendar" "Contacts" "Dock" "Finder" "Mail" "Safari" "SystemUIServer" "TextEdit"; do
+for app in "Dock" "Finder" "SystemUIServer"; do
   killall "${app}" &>/dev/null
 done
