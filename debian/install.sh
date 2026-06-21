c7d4743b24b3b7e2cabceac2c02c69726a63f9f0
parser <boogie-rover.0a@icloud.com>
2026-06-20 19:21:53 -0500

    Update debian/install.sh

--- debian/install.sh
+++ debian/install.sh
@@ -12,14 +12,9 @@ curl -f https://zed.dev/install.sh | sh
 
 # Install FiraCode Nerd Font for user only
 mkdir -p ~/.local/share/fonts/FiraCode
-wget -P ~/.local/share/fonts/FiraCode https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip
+wget -P ~/.local/share/fonts/FiraCode "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip"
 
 # Unzip, delete archive, and refresh font cache
 unzip ~/.local/share/fonts/FiraCode/FiraCode.zip -d ~/.local/share/fonts/FiraCode/
 rm ~/.local/share/fonts/FiraCode/FiraCode.zip
 fc-cache -fv
-
-# https://gitlab.com/Schwarzer_Kater
-# https://github.com/DavidoTek/ProtonUp-Qt
-# https://github.com/VirusAlex/elden-proton
-# https://github.com/PaulCombal/SamRewritten
