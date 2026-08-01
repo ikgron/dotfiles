4c4ad1ce410d8f04fae2c6d5714d00b64fd1f234
parser <boogie-rover.0a@icloud.com>
2026-08-01 17:59:41 -0500

    Small update

--- debian/install.sh
+++ debian/install.sh
@@ -8,28 +8,25 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y --ignore-missing btop curl eza fontconfig gamemode git git-delta gpg starship unzip wget
 
 # Ghostty
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)"
+sudo add-apt-repository ppa:mkasberg/ghostty-ubuntu
+sudo apt install ghostty
 
 # Fastfetch
-curl -Lo /tmp/fastfetch-latest.deb \
-    https://github.com/fastfetch-cli/fastfetch/releases/latest/download/fastfetch-linux-amd64.deb
-
-sudo dpkg -i /tmp/fastfetch-latest.deb
-
-rm /tmp/fastfetch-latest.deb
+sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
+sudo apt install fastfetch
 
 # Recommended Zed install
 curl -fsSL https://zed.dev/install.sh | sh
 
 # Install VSCodium
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg |
-    gpg --dearmor |
-    sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
 
-echo -e 'Types: deb\nURIs: https://download.vscodium.com/debs\nSuites: vscodium\nComponents: main\nArchitectures: amd64 arm64\nSigned-by: /usr/share/keyrings/vscodium-archive-keyring.gpg' |
-    sudo tee /etc/apt/sources.list.d/vscodium.sources
+echo -e 'Types: deb\nURIs: https://download.vscodium.com/debs\nSuites: vscodium\nComponents: main\nArchitectures: amd64 arm64\nSigned-by: /usr/share/keyrings/vscodium-archive-keyring.gpg' \
+| sudo tee /etc/apt/sources.list.d/vscodium.sources
 
-sudo apt update && sudo apt install -y codium
+sudo apt install codium
 
 # Install FiraCode Nerd Font for user only
 FONT_DIR="$HOME/.local/share/fonts/FiraCode"
